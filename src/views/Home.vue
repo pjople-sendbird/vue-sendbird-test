8e2aeda57445ce77ccc891cfdf1dfbc637faadfa
Walter Rodriguez (Sendbird) <walter.rodriguez@sendbird.com>
2021-03-22 11:40:38 +0000

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+/**
+ * Sendbird
+ * You should create a class but for this simple test, 
+ * I will place all here in this file.
+ */
+import Sendbird from "sendbird";
+var APP_ID = "D1CB1742-A4A3-44B9-9E7F-126D14BAB34B"; // This is your Sendbird Application ID (https://dashboard.sendbird.com)
+var USER_ID = "test2"; // Any user_id from your account
+var ACCESS_TOKEN = null;  // Users can have access tokens 
+const UNIQUE_HANDLER_ID = "ANY-IDENTIFIER-HERE";  // Any identifier for your handlers (https://sendbird.com/docs/chat/v3/javascript/guides/event-handler)
+
+/**
+ * Initialize Sendbird
+ */
+var sb = new Sendbird({ appId: APP_ID });
+
+/**
+ * Connect to Websockets
+ */
+sb.connect(USER_ID, ACCESS_TOKEN, (user, error) => {
+  /**
+   * You have a User object
+   */
+  console.dir(user);
+  /**
+   * Create a channel handler to 
+   * receive events for messages 
+   * and channels
+   */
+  var channelHandler = new sb.ChannelHandler();
+  channelHandler.onMessageReceived = function (channel, message) {
+    /**
+     * New message arrived
+     */
+    console.log("onMessageReceived");
+    console.dir(message);
+  };
+  sb.addChannelHandler(UNIQUE_HANDLER_ID, channelHandler);
+});
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
